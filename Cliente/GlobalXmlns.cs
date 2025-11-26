c8d54a06dafb4bc3180bc80c5a71e7acaf90dbbd
rusok <rusok.software@gmail.com>
2025-11-26 19:16:58 -0400

    Agregar archivos de proyecto.

---
+++ Cliente/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Cliente")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Cliente.Pages")]
