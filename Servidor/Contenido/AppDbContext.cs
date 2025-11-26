a3d6b760625bd198f60a81c44c6f8b1139b95f65
rusok <rusok.software@gmail.com>
2025-11-26 19:34:51 -0400

    Configurando SQLite

---
+++ Servidor/Contenido/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Servidor.Contenido
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Servidor.Models.Plato> Platos => Set<Servidor.Models.Plato>();
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        
+    }
+}
--- Servidor/Program.cs
+++ Servidor/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Servidor.Contenido;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("MiConexionLocalSQLite")));
 var app = builder.Build();
 app.Run();
