9603a7c3c35c25ef2165eb3930c2eccebd6cbdfd
rusok <rusok.software@gmail.com>
2025-11-27 19:52:54 -0400

    Preparando conexión HTTP

---
+++ Cliente/ConexionDatos/RestConexionDatos.cs
@@ -0,0 +1,42 @@
+﻿using Cliente.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Cliente.ConexionDatos
+{
+    public class RestConexionDatos : IRestConexionDatos
+    {
+        public readonly HttpClient httpClient;
+        private readonly string dominio;
+        private readonly string url;
+        private readonly JsonSerializerOptions jsonSerializerOptions;
+        public RestConexionDatos()
+        {
+            httpClient = new HttpClient();
+            dominio = DeviceInfo.Platform == DevicePlatform.Android? "http://10.0.2.2:5018": "http://localhost:5018";
+        }
+        public Task AddPlato(Plato plato)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeletePlato(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Plato>> ObtenerPlatos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdatePlato(Plato plato)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Cliente/Models/Plato.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente.Models
+{
+    public class Plato
+    {
+        private int _id;
+        public int id
+        {
+            get { return _id; }
+            set {
+                if(_id == value) 
+                    return;
+                _id = value;
+                PropertyChanged?.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(nameof(id)));
+            }
+        }
+        private string _nombre;
+        public string nombre
+        {
+            get { return _nombre; }
+            set {
+                if(_nombre == value) 
+                    return;
+                _nombre = value;
+                PropertyChanged?.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(nameof(nombre)));
+            }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
